0abb198324018e76dd6ee486abbcfb1cf480f911
Mizuki <mbaba@kanolab.net>
2023-09-19 10:27:03 +0900

    [add] sanity check and can return result for bool

--- originality_ditection/detector.py
+++ originality_ditection/detector.py
@@ -1,3 +1,5 @@
+from transformers.utils.logging import os
+
 from originality_ditection import model
 
 
@@ -8,18 +10,45 @@ class detector:
         self.model_path = model_path
         self.ai_degree = ai_degree
         self.human_degree = human_degree
+        self.detect_model = model.Model(self.model_path)
+        self.sanity_check()
+
+    def sanity_check(self) -> None:
+        if os.path.exists(self.model_path) == False:
+            raise ValueError("model_path is not found.")
+
+        # Check ai_degree and human_degree.
+        if self.ai_degree < 0.0 or self.ai_degree > 1.0:
+            raise ValueError("ai_degree must be between 0.0 and 1.0.")
+        if self.human_degree < 0.0 or self.human_degree > 1.0:
+            raise ValueError("human_degree must be between 0.0 and 1.0.")
 
     def detect(
         self,
         question: str,
         answer: str,
     ) -> str:
-        detect_model = model.Model(self.model_path)
-        prob_human = detect_model.predict(question, answer)
-        print(f"Probability of human answers: {prob_human}")
+        prob_human = self.detect_model.predict(question, answer)
+        return_text = (
+            f"======= \n Probability of human answers: {prob_human} \n ======= \n"
+        )
         if prob_human > self.human_degree:
-            return "it considered to be a human answer"
-        elif prob_human < 0.1:
-            return "it considered to be an AI answer"
+            return_text += "it considered to be a human answer"
+        elif prob_human < self.ai_degree:
+            return_text += "it considered to be an AI answer"
         else:
-            return "can't determine if it's a human or an AI answer"
+            return_text += "can't determine if it's a human or an AI answer"
+        return return_text
+
+    def detect_bool(
+        self,
+        question: str,
+        answer: str,
+    ) -> bool or None:
+        prob_human = self.detect_model.predict(question, answer)
+        if prob_human > self.human_degree:
+            return True
+        elif prob_human < self.ai_degree:
+            return False
+        else:
+            return None
--- originality_ditection/detector_sample.py
+++
@@ -1,7 +0,0 @@
-from originality_ditection import detector
-
-question = "インターンシップの志望動機"
-answer = "インターンシップの志望動機は、自分のスキルを活かして、会社の発展に貢献したいからです。"
-detector = detector("model/model.pkl")
-parse_text = detector.detect(question, answer)
-print(parse_text)
